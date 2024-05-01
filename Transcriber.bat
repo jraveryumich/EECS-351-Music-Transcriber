cb28346a326f9568158688937eee7db9eb467f7c
jraveryumich <90657031+jraveryumich@users.noreply.github.com>
2024-05-01 11:48:53 -0400

    Delete Transcriber.bat

--- Transcriber.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-"C:\Users\jrave\Desktop\Junior 2023\EECS 351\Final Project\My Attempt\venv\Scripts\python.exe" "C:/Users/jrave/Desktop/Junior 2023/EECS 351/Final Project/My Attempt/main.py"
